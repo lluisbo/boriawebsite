c2b638515f7b8f629feb34ced8b53aaa1592b8f5
Lluis Boria <lluisb.gomez@gmail.com>
2021-02-23 18:44:17 +0100

    no message

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require('express')
 const cors = require('cors')
-require('./src/mongo/dbconnection')
+const passport = require('passport')
+const bodyParser = require('body-parser')
 
+require('./src/authMiddleware/auth')
+const authRoute = require('./src/routes/auth')
 const tourDatesRoutes = require('./src/routes/tourDates')
+const profileRoute = require('./src/routes/profile')
 
 const app = express()
 const Port = 3001
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors({
   origin: '*',
   optionsSuccessStatus: 200
 }))
 
+app.use('/', authRoute)
+app.use('/user', passport.authenticate('jwt', { session: false }), profileRoute)
 app.use('/tourdates', tourDatesRoutes)
+
+// HANDLE ERRORS
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({ error: err })
+})
 
 app.listen(Port, () => {
   console.log(`App running on port ${Port}`)
--- src/mongo/models/Users.js
+++ src/mongo/models/Users.js
@@ -1,61 +1,32 @@
-
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
-const ThirdPartyProviderSchema = new mongoose.Schema({
-  provider_name: {
-    type: String,
-    default: null
-  },
-  provider_id: {
-    type: String,
-    default: null
-  },
-  provider_data: {
-    type: {},
-    default: null
-  }
-})
-
-// Create Schema
 const schema = new mongoose.Schema({
-  name: {
-    type: String
-  },
   email: {
     type: String,
     required: true,
     unique: true
   },
-  email_is_verified: {
-    type: Boolean,
-    default: false
-  },
   password: {
-    type: String
-  },
-  referral_code: {
     type: String,
-    default: function () {
-      let hash = 0
-      for (let i = 0; i < this.email.length; i++) {
-        hash = this.email.charCodeAt(i) + ((hash << 5) - hash)
-      }
-      const res = (hash & 0x00ffffff).toString(16).toUpperCase()
-      return '00000'.substring(0, 6 - res.length) + res
-    }
-  },
-  referred_by: {
-    type: String,
-    default: null
-  },
-  third_party_auth: [ThirdPartyProviderSchema],
-  date: {
-    type: Date,
-    default: Date.now
+    required: true
   }
-},
-{ strict: false }
+})
+schema.pre(
+  'save',
+  async function (next) {
+    // const User = this
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    next()
+  }
 )
+schema.methods.isValidPassword = async function (password) {
+  const user = this
+  const compare = await bcrypt.compare(password, user.password)
+  return compare
+}
 
-const User = mongoose.model('tourdate', schema)
+const User = mongoose.model('user', schema)
+
 module.exports = User
